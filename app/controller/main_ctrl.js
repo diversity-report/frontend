635096c84120ebaee553489e7044de542c8fa6da
Aliza Pilisuk <apilisuk@gmail.com>
2016-11-05 20:09:53 -0700

    charts are displaying! Hard coded data but commented out dynamic variables to use later

--- app/controller/main_ctrl.js
+++ app/controller/main_ctrl.js
@@ -4,7 +4,6 @@ require('angular-chart.js');
 
 // const app = angular.module('drApp', ['chart.js']);
 
-
 module.exports = (app) => {
   app.controller('mainController', ['$http','$scope', function($http,$scope){
     $scope.information = {};
@@ -19,18 +18,30 @@ module.exports = (app) => {
       console.log('Company Data Object: ',data);
       let genderLabels = ['Not Reported', 'Non-Binary', 'Male', 'Female'];
       let raceLabels = ['Arab/Middle Eastern', 'Asian/Pacific Islander', 'Black/African American', 'Latino/Hispanic', 'Native American/Alaskan Native', 'Native Hawaiian/Pacific Islander', 'White/Caucasian', 'Not Reported'];
-      let dependantLabel = ['Dependants', 'No Dependants'];
-      let veteranLabels = ['Veteran', 'Not Veteran'];
+      let dependantsLabels = ['Dependants', 'No Dependants'];
+      let veteransLabels = ['Veteran', 'Not Veteran'];
       let nonVeteran = $scope.information.numOfEmployees - $scope.information.veteran;
       let veteranData = [$scope.information.veteran, nonVeteran];
       let noDependants = $scope.information.numOfEmployees - $scope.information.dependants;
-      let dependantData = [$scope.information.dependants, noDependants];
+      let dependantsData = [$scope.information.dependants, noDependants];
       let genderData = [$scope.information.gender.unreported, $scope.information.gender.nonbinary, $scope.information.gender.male, $scope.information.gender.female];
       let raceData = [$scope.information.race.arabMiddleEastern, $scope.information.race.asianPacificIslander, $scope.information.race.blackAfricanAmerican, $scope.information.race.latinoHispanic, $scope.information.race.nativeAmericanAlaskanNative, $scope.information.race.nativeHawaiianPacificIslander, $scope.information.race.whiteCaucasian, $scope.information.race.unreported];
       console.log('Gender Labels: ' + genderLabels + ', Gender Data: ' + genderData);
       console.log('Race Labels: ' + raceLabels + ', Race Data: ' + raceData);
-      console.log('Veteran Labels: ' + veteranLabels + ', Veteran Data: ' + veteranData);
-      console.log('Dependant Labels: ' + dependantLabel + ', Dependant Data: ' + dependantData);
+      console.log('Veteran Labels: ' + veteransLabels + ', Veteran Data: ' + veteranData);
+      console.log('Dependant Labels: ' + dependantsLabels + ', Dependant Data: ' + dependantsData);
+      $scope.genderData = [5,2,3,7];
+      // $scope.genderData = genderData;
+      $scope.genderLabels = genderLabels;
+      $scope.raceData = [5,2,3,7,4,6,1,9];
+      // $scope.raceData = raceData;
+      $scope.raceLabels = raceLabels;
+      $scope.veteransData = [5,2];
+      // $scope.veteranData = veteranData;
+      $scope.veteransLabels = veteransLabels;
+      $scope.dependantsData = [5,2];
+      // $scope.dependantsData = dependantsData;
+      $scope.dependantsLabels = dependantsLabels;
     })
       .error(function(data) {
         console.log('Error: ' + data);
