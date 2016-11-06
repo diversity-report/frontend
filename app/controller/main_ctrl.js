67375ef2b48efa7d9eb04f7216502debb462456c
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-11-06 11:26:30 -0800

    not working!!!!

--- app/controller/main_ctrl.js
+++ app/controller/main_ctrl.js
@@ -1,18 +1,45 @@
 'use strict';
 
 require('angular-chart.js');
-
+// const _ = require('lodash');
 module.exports = (app) => {
   app.controller('mainController', ['$http','$scope', function($http,$scope){
     $scope.information = {};
     $scope.newCompany = {};
-  // this.getOneCompany = function(companyId){
+    // $scope.searchSubmit = function(){
+    //   var data = $.param({
+    //     json: JSON.stringify({
+    //
+    //     })
+    //   })
+    // }
+    var dataUrl = `${__API_URL__}/api/company`;
+    $http.get(dataUrl)
+    .success(function(data) {
+      $scope.bigData = data;
+      $scope.city = [];
+      $scope.seachFilter = function(companyName){
+        // for(var i=0; i < $scope.bigData.length; i++){
+        //   if($scope.bigData[i].city == )
+        // }
+        $scope.unfilteredData = $scope.unfilteredData || $scope.bigData;
+        $scope.bigData = $scope.unfilteredData.filter((item)=>{
+          return item.city.includes(`${companyName}`);
+        })
+      };
+    });
+
+
+    // $scope.getOneCompany = function(companyId){
     // var url = `${__API_URL__}/api/company/${companyId}`;
-    var url = `${__API_URL__}/api/company/581f41b86b9cf347580994b1`;
+    var url = `${__API_URL__}/api/company/581e6b0fe30adb51f67eab67`;
     $http.get(url)
     .success(function(data) {
+      // $scope.searchSubmit = function(data){
+      //
+      // };
+
       $scope.information = data;
-      // $scope.labels =
       console.log('Company Data Object: ',data);
       let genderLabels = ['Not Reported', 'Non-Binary', 'Male', 'Female'];
       let raceLabels = ['Arab/Middle Eastern', 'Asian/Pacific Islander', 'Black/African American', 'Latino/Hispanic', 'Native American/Alaskan Native', 'Native Hawaiian/Pacific Islander', 'White/Caucasian', 'Not Reported'];
@@ -40,6 +67,7 @@ module.exports = (app) => {
       // $scope.dependantsData = [5,2];
       $scope.dependantsData = dependantsData;
       $scope.dependantsLabels = dependantsLabels;
+      $scope.info =[];
     })
       .error(function(data) {
         console.log('Error: ' + data);
@@ -49,7 +77,7 @@ module.exports = (app) => {
   // $scope.createCompany = function() {
   //   var url = `${__API_URL__}/api/newCompany/`;
   //   $http.post(url)
-  //     .success(function(data) {
+  //     .success(function($scope.newCompany) {
   //       $scope.newCompany = data; // clear the form so our user is ready to enter another
   //       $scope.data = data;
   //       console.log('%%%%%%%%%%',data);
@@ -62,6 +90,5 @@ module.exports = (app) => {
     $scope.createCompany = function(data){//delete
       console.log('data', data );
     };
-
   }]);
 };
