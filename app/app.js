243d0fc65a1a665e08e9020ec2283bdfbacabc67
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-11-05 20:13:08 -0700

    add search html and route

--- app/app.js
+++ app/app.js
@@ -3,9 +3,10 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-const app = angular.module('drApp', []);
+const ngRoute = require('angular-route');
+const app = angular.module('drApp', [ngRoute]);
 
-require('./controller/main_ctrl.js');
+require('./controller/main_ctrl.js')(app);
 
 app.config(['$routeProvider', ($routeProvider) => {
   $routeProvider
@@ -14,6 +15,9 @@ app.config(['$routeProvider', ($routeProvider) => {
   })
   .when('/contribute', {
     template: require('./templates/contribute.html')
+  })
+  .when('/search',{
+    template: require('./templates/search.html')
   })
   .otherwise({
     redirectTo: '/'
