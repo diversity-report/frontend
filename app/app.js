b2ae5202ecafe9c9a5d0038508723e0e49985e47
Sawako <sawako.yoriko@gmail.com>
2016-11-05 20:36:21 -0700

    form, ng-view in html, main controller modified

--- app/app.js
+++ app/app.js
@@ -3,19 +3,17 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-const app = angular.module('drApp', []);
+const ngRoute = require('angular-route');
+const app = angular.module('drApp', [ngRoute]);
 
-require('./controller/main_ctrl.js');
+require('./controller/main_ctrl.js')(app);
 
-app.config(['$routeProvider', ($routeProvider) => {
+app.config(['$routeProvider', function($routeProvider){
   $routeProvider
   .when('/', {
     template: require('./html/index.html')
   })
   .when('/contribute', {
     template: require('./templates/contribute.html')
-  })
-  .otherwise({
-    redirectTo: '/'
   });
 }]);
--- app/controller/main_ctrl.js
+++ app/controller/main_ctrl.js
@@ -1,19 +1,23 @@
 'use strict';
-const angular = require('angular');
-const app = angular.module('drApp', []);
+module.exports = (app) => {
+  app.controller('mainController', [ '$http','$scope', function($http, $scope){
+    $scope.data = {};
+    $scope.newCompany = {};//delete
+    
+    // this.getOneCompany = function(companyId){
+      // var url = `${__API_URL__}/api/company/${companyId}`;
+    // var url = `${__API_URL__}/api/company/581e6b0fe30adb51f67eab67`;
+    // $http.get(url)
+    //   .success(function(data) {
+    //     $scope.data = data;
+    //     console.log('@@@@@@@@@@@@@@@@@@',data);
+    //   })
+    //     .error(function(data) {
+    //       console.log('Error: ' + data);
+    //     });
 
-app.controller('mainController', [ '$http','$scope', function($http,$scope){
-  $scope.data = {};
-  // this.getOneCompany = function(companyId){
-    // var url = `${__API_URL__}/api/company/${companyId}`;
-  var url = `${__API_URL__}/api/company/581e6b0fe30adb51f67eab67`;
-  $http.get(url)
-    .success(function(data) {
-      $scope.data = data;
-      console.log('@@@@@@@@@@@@@@@@@@',data);
-    })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-
-}]);
+    $scope.createCompany = function(data){//delete
+      console.log('data', data );
+    };
+  }]);
+};
